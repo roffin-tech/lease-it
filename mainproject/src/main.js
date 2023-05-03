3918bcbbbbb7766bd04f506ac95062828e25a7f4
Roffin <roffin@techgentsia.com>
2023-05-04 00:22:46 +0530

    env updates

--- mainproject/src/main.js
+++ mainproject/src/main.js
@@ -1,10 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import axios from 'axios'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import axios from "axios";
 
+axios.defaults.baseURL =
+  process.env.VUE_APP_API_BASE_URL || "http://localhost:8094";
 
-axios.defaults.baseURL = 'http://localhost:8094'
-
-createApp(App).use(store).use(router, axios).mount('#app')
+createApp(App).use(store).use(router, axios).mount("#app");
