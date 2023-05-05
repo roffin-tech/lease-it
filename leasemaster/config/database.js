f9a14b2b92f380efba0b7f1aa26d6c1395c99a8c
Roffin <roffin@techgentsia.com>
2023-05-05 08:01:51 +0530

    db update

--- leasemaster/config/database.js
+++ leasemaster/config/database.js
@@ -3,14 +3,16 @@ import mysql from "mysql2";
 // create the connection to database
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || "localhost",
-    user:  process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_DATABASE || "test-dab1"
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_DATABASE || "test-dab1",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-
-db.connect(error => {
+db.connect((error) => {
   if (error) throw error;
   console.log("Successfully connected to the database.");
 });
